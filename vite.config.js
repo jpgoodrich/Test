9e05b633b53709c9eeb098733141b3b7a01bc816
James Goodrich <jgoodrich@esri.com>
2025-02-19 16:14:37 -0800

    chore: wip

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        globals: { crypto: 'crypto' },
+      },
+      external: ['crypto'],
+      plugins: [nodeResolve({ preferBuiltins: true })],
+    },
+  },
 })
